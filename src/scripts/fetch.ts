e65aa4be456d6ca2d47ac898f907115b71c66ab7
Kyle Scott <kyscott@umich.edu>
2022-05-17 10:20:19 -0400

    fetch mobius pools from static config

--- src/scripts/fetch.ts
+++ src/scripts/fetch.ts
@@ -1,7 +1,11 @@
+import { fetchMobius } from "./fetchMobius";
+import { fetchUbeswap } from "./fetchUbeswap";
 import { fetchUniswap } from "./fetchUniswapV3";
 
 export const fetch = async (): Promise<void> => {
-  await fetchUniswap();
+  await Promise.all(
+    [fetchUniswap, fetchUbeswap, fetchMobius].map((fet) => fet())
+  );
 };
 
 fetch().catch((err) => {
--- src/scripts/fetchMobius.ts
+++ src/scripts/fetchMobius.ts
@@ -1,24 +1,20 @@
-import { CUSD, UST } from "@dahlia-labs/celo-tokens";
+import { ChainId } from "@dahlia-labs/celo-contrib";
+import { getAddress } from "@ethersproject/address";
 import * as fs from "fs/promises";
 
 import type { Pool } from "..";
 import { Exchange } from "..";
+import { StablePools } from "./mobiusData/pools";
 
 export const fetchMobius = async (): Promise<void> => {
-  // const rawFile = await fs.readFile("data/pools.json");
+  const pools: Pool[] = StablePools[ChainId.Mainnet].map((p) => ({
+    chainID: ChainId.Mainnet,
+    exchange: Exchange.Saddle,
+    tokens: p.pool.tokens.map((t) => t),
+    address: getAddress(p.pool.address),
+  }));
 
-  // const pools: Pool[] = JSON.parse(rawFile.toString()) as Pool[];
-
-  const pools: Pool[] = [
-    {
-      chainID: 42220,
-      exchange: Exchange.Saddle,
-      tokens: [CUSD[42220], UST[42220]],
-      address: "0x9F4AdBD0af281C69a582eB2E6fa2A594D4204CAe",
-    },
-  ];
-
-  await fs.writeFile("src/data/pools.json", JSON.stringify(pools, null, 2));
+  await fs.writeFile("src/data/mobius.json", JSON.stringify(pools, null, 2));
 
   console.log(`Discovered and wrote ${pools.length} pools`);
 };
