e608278207a54af485a44853799eb41c71e539ed
Kyle Scott <kyscott@umich.edu>
2022-05-19 11:31:50 -0400

    endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import type { Token } from "@dahlia-labs/token-utils";
+import axios from "axios";
+import fs from "fs";
 
 export enum Exchange {
   UniswapV2,
@@ -17,3 +19,18 @@ export interface Pool {
   tokens: Token[];
   address: string;
 }
+
+export const remotePath =
+  "https://raw.githubusercontent.com/DahliaLabs/minima-pair-registry/master/src/data/";
+
+const localPath = "src/data";
+
+export const fetchPools = async (): Promise<Pool[]> => {
+  const dir = fs.readdirSync(localPath);
+  const poolData = (
+    await Promise.all(
+      dir.flatMap(async (d) => axios.get<Pool[]>(remotePath.concat(d)))
+    )
+  ).flatMap((p) => p.data);
+  return poolData;
+};
